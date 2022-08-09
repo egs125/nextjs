94012903ca0c7b3f3b750607ef11fcf4750519c7
egs125 <egs125@gmail.com>
2022-08-10 06:45:08 +0900

    style jsx

--- pages/NavBar.js
+++ pages/NavBar.js
@@ -7,10 +7,21 @@ export default function NavBar() {
 
     return <nav>
         <Link href="/">
-          <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>HOME</a>
+          <a className={router.pathname === '/' ? 'active' : ''}>HOME</a>
         </Link>
         <Link href="/about">
-          <a className={[ styles.link, router.pathname === "/about" ? styles.active : ""].join(" ")}>ABOUT</a>
+          <a className={router.pathname === '/about' ? 'active' : ''}>ABOUT</a>
         </Link>
+        <style jsx>{`
+          nav {
+            background-color: tomato;
+          }
+          a {
+            text-decortion: none;
+          }
+          .active {
+            color: yellow;
+          }
+        `}</style>
     </nav>;
 };
